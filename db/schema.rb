c689c583d41e7d203777de6d641209b77755bd22
ruben.assouline <ruben.assouline@tuteurprive.com>
2018-03-18 11:58:17 -0400

    cration page utilisatuer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315221338) do
+ActiveRecord::Schema.define(version: 20180318150247) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20180315221338) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "phone_number"
+    t.text     "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
