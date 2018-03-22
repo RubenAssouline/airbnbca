a41feeaf31d039de7de25abb7a4aad7e99ca9141
ruben.assouline <ruben.assouline@tuteurprive.com>
2018-03-22 10:56:04 -0400

    Form ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ActiveRecord::Base
   validates :fullname, presence:true, length: {maximum: 50}
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/asset/default_image.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  has_many : romms 
+  has_many :rooms 
 end
